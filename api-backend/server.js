fc7285e3274fa90bd65a7d242700d5a17651f7f7
Charis Pipis <el18240@mail.ntua.gr>
2023-01-26 23:37:51 +0200

    Implement config files and server.js

--- api-backend/server.js
+++ api-backend/server.js
@@ -1,9 +1,30 @@
-const app = require('./app');
+const express = require('express')
+const app = express()
 
-const chalk = require("chalk");
+const chalk = require("chalk")
 
-const port = process.env.SERVER_PORT || 3000;
+const indexRouter = require('./routes/index')
+
+// INSTALL AND UNCOMMENT IF TEMPLATES/LAYOUTS ARE NEEDED
+// const expressLayouts = require('express-ejs-layouts')
+// app.set('view engine', 'ejs')
+// app.set('views', __dirname + '/views')
+// app.set('layout', 'layouts/layout')
+// app.use(expressLayouts)
+app.use(express.static('public'))
+
+app.use('/', indexRouter)
+
+const port = process.env.PORT || 9103;
+const baseurl = '/intelliq_api'
+
+// TODO: add support for https
 
 app.listen(port, () => {
-    console.log(chalk.green(`🚀 Server running on port ${port}!`));
+    console.log(chalk.green(`🚀 Server running at: http://localhost:${port}${baseurl}`));
 });
+
+const healthcheck = require('./api/admin/healthcheck')
+
+app.use(baseurl + '/admin/healthcheck', healthcheck)
+
