15c6dd73317c66a5eae42cd98148b6f29acc9251
Charilaos Pipis <el18240@mail.ntua.gr>
2023-02-14 01:22:35 +0200

    Implement transactions in the DB access of questionnaire_upd and correct some success response codes

--- api-backend/api/admin/questionnaire_upd.js
+++ api-backend/api/admin/questionnaire_upd.js
@@ -23,23 +23,29 @@ router.post('/', multer().any(), async (req, res) => {
     "Successful upload of new questionnaire!", async (conn) => {
         const json_new_quest = JSON.parse(req.files[0].buffer.toString());
 
-        // TODO: validate data? Or maybe just implement all
-        // following SQL queries using a transaction
+        try {
+            await conn.beginTransaction();
 
-        await conn.query(
-            "INSERT INTO questionnaire (questionnaire_id, title) VALUES (?, ?)",
-            [json_new_quest.questionnaireID, json_new_quest.questionnaireTitle]
-        );
-
-        for (keyword of json_new_quest.keywords) {
             await conn.query(
-                "INSERT INTO keywords (questionnaire_id, word) VALUES (?, ?)",
-                [json_new_quest.questionnaireID, keyword]
+                "INSERT INTO questionnaire (questionnaire_id, title) VALUES (?, ?)",
+                [json_new_quest.questionnaireID, json_new_quest.questionnaireTitle]
             );
-        }
 
-        for (question of json_new_quest.questions) {
-            await insertQuestion(conn, question, json_new_quest.questionnaireID);
+            for (keyword of json_new_quest.keywords) {
+                await conn.query(
+                    "INSERT INTO keywords (questionnaire_id, word) VALUES (?, ?)",
+                    [json_new_quest.questionnaireID, keyword]
+                );
+            }
+
+            for (question of json_new_quest.questions) {
+                await insertQuestion(conn, question, json_new_quest.questionnaireID);
+            }
+
+            await conn.commit();
+        } catch (err) {
+            conn.rollback();
+            throw err;
         }
     });
 });
