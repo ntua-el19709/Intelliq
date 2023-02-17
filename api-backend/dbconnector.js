965df209a344a0cbd4461d29e502dcc380c234fd
Charilaos Pipis <el18240@mail.ntua.gr>
2023-02-17 13:05:07 +0200

    Comment out test DB

--- api-backend/dbconnector.js
+++ api-backend/dbconnector.js
@@ -1,16 +1,16 @@
 const mysql = require('promise-mysql');
 
-if (process.env.NODE_ENV === "test") {
-    const pool = mysql.createPool({
-        host: "localhost",
-        user: "softenguser",
-        password: "password",
-        database: "intelliqdb_test",
-        charset: 'utf8mb4',
-        connectionLimit: 100,
-        multipleStatements: true
-    });
-} else {
+// if (process.env.NODE_ENV === "test") {
+//     const pool = mysql.createPool({
+//         host: "localhost",
+//         user: "softenguser",
+//         password: "password",
+//         database: "intelliqdb_test",
+//         charset: 'utf8mb4',
+//         connectionLimit: 100,
+//         multipleStatements: true
+//     });
+// } else {
     const pool = mysql.createPool({
         host: "localhost",
         user: "softenguser",
@@ -20,6 +20,6 @@ if (process.env.NODE_ENV === "test") {
         connectionLimit: 100,
         multipleStatements: true
     });
-}
+// }
 
 module.exports = pool;
